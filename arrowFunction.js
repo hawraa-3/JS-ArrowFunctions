3a33f3131631d2bd42e44894d7a283584f0700dd
hawraa-3 <hawraaalmahdi.33@gmail.com>
2024-11-05 19:53:32 +0300

    Finally

---
+++ arrowFunction.js
@@ -0,0 +1,25 @@
+// Rewrite the following function as an arrow function:
+/******************************
+	function greet(name) {
+	  return `Hello, ${name}!`;
+	}
+********************************/
+
+const greet = (name) => `Hello, ${name}!`;
+
+
+console.log(greet("Hawraa"));
+
+
+// Write a simple arrow function that takes two parameters and returns their sum.
+
+const sum =(a,b) => a+b;
+
+console.log(sum(5,3));
+
+
+// Write a simple arrow function that squares a number.
+
+const squares = (number) => number**2;
+
+console.log(squares(2));
